b7105190967b8bca400c10facad96fd42c022c0d
ABINEL MWANGI <kiraguabinel@gmail.com>
2023-05-17 15:24:04 +0300

    Complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  const sourceImage ={
+    alt:"I made this"
+    };
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I like the new Joefes' jam</p>
+    <img  src={image} alt={sourceImage.alt}/>
+  </div>);
 }
 
 export default About;
